50201703128c986d523a1f3e8cf07cc7322d03ca
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2022-08-11 23:12:05 +0500

    Refactor flying eye patrolling

--- Assets/Scripts/Flying eye/FlyingEyeMovement.cs
+++ Assets/Scripts/Flying eye/FlyingEyeMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Flying_eye
@@ -5,8 +6,8 @@ namespace Flying_eye
     public class FlyingEyeMovement : MonoBehaviour
     {
         [SerializeField] private float flySpeed = 30f;
-        [SerializeField] private Transform[] wayPoints;
-        
+        [SerializeField] private Transform route;
+
         public delegate void OnMoveRight();
 
         public event OnMoveRight MoveRight;
@@ -15,13 +16,25 @@ namespace Flying_eye
 
         public event OnMoveLeft MoveLeft;
 
+        private List<Vector3> _wayPoints;
         private int _currentWayPointIndex;
+        private bool _isOnTheWayBack;
+
+        private void Start()
+        {
+            _wayPoints = new List<Vector3>();
+            for (var i = 0; i < route.childCount; i++)
+            {
+                _wayPoints.Add(route.GetChild(i).transform.position);
+            }
+            Debug.Log(_wayPoints.Count);
+        }
 
         private void Update()
         {
-            if (wayPoints.Length == 0) return;
+            if (_wayPoints.Count == 0) return;
 
-            if (transform.position.x < wayPoints[_currentWayPointIndex].position.x)
+            if (transform.position.x < _wayPoints[_currentWayPointIndex].x)
             {
                 MoveRight?.Invoke();
             }
@@ -29,18 +42,28 @@ namespace Flying_eye
             {
                 MoveLeft?.Invoke();
             }
-            transform.position = Vector3.MoveTowards(transform.position, wayPoints[_currentWayPointIndex].position,
+
+            transform.position = Vector3.MoveTowards(transform.position, _wayPoints[_currentWayPointIndex],
                 flySpeed * Time.deltaTime);
-        
-            if (transform.position == wayPoints[_currentWayPointIndex].position)
+
+            if (transform.position == _wayPoints[_currentWayPointIndex])
             {
-                if (_currentWayPointIndex + 1 < wayPoints.Length)
+                if (_isOnTheWayBack)
                 {
-                    _currentWayPointIndex++;
+                    _currentWayPointIndex--;
                 }
                 else
                 {
-                    _currentWayPointIndex = 0;
+                    _currentWayPointIndex++;
+                }
+
+                if (!_isOnTheWayBack && _currentWayPointIndex == _wayPoints.Count - 1)
+                {
+                    _isOnTheWayBack = true;
+                }
+                else if (_isOnTheWayBack && _currentWayPointIndex == 0)
+                {
+                    _isOnTheWayBack = false;
                 }
             }
         }
